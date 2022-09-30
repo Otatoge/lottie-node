2eeb14317f95a7064794cd99134c6fb03ce8dec1
Otatoge <104771756+Otatoge@users.noreply.github.com>
2022-10-01 08:03:48 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('node:fs');
+
+require('jsdom-global')();
+
+const lottieScript = fs.readFileSync(require.resolve('lottie-web/build/player/lottie.min'), 'utf8');
+
+new Function(lottieScript)();
+
+/**
+ * @type {import('lottie-web').LottiePlayer}
+ */
+module.exports = lottie
