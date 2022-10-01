21971c9cec012b0d93c20ff0a47a9ed34adaeeb0
Koro soft <104771756+Otatoge@users.noreply.github.com>
2022-10-01 16:37:06 +0900

    It's a destructive change!

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ require('jsdom-global')();
 
 const lottieScript = fs.readFileSync(require.resolve('lottie-web/build/player/lottie.min'), 'utf8');
 
-/**
- * @type {import('lottie-web').LottiePlayer}
- */
-let lottie
-
 new Function(lottieScript)();
 
-module.exports = lottie
+/**
+ * Returns an object in lottie-web
+ * @returns {import('lottie-web').LottiePlayer}
+ */
+module.exports = () => lottie
