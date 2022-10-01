d0015221bc464eca8c19d210513724952b059fd9
Koro soft <104771756+Otatoge@users.noreply.github.com>
2022-10-01 10:48:06 +0900

    update

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ require('jsdom-global')();
 
 const lottieScript = fs.readFileSync(require.resolve('lottie-web/build/player/lottie.min'), 'utf8');
 
-new Function(lottieScript)();
-
 /**
- * Lottie
  * @type {import('lottie-web').LottiePlayer}
  */
+let lottie
+
+new Function(lottieScript)();
+
 module.exports = lottie
